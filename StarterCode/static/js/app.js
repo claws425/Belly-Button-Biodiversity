4a2ad4e3ab29d4f958f8c8850e186b9187dc3b59
claws425 <73760859+claws425@users.noreply.github.com>
2021-04-25 16:48:02 -0400

    working on app code

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,7 @@
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      // Filter the data for the object with the desired sample number
+      var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+      var result = resultArray[0];
+     
