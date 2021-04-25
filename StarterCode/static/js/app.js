b84005301db85ab97ab49a2d0900090cb561c3d4
claws425 <73760859+claws425@users.noreply.github.com>
2021-04-25 17:07:05 -0400

    added variables to function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -20,3 +20,11 @@ function buildMetadata(sample) {
 }
 
 function buildCharts(sample) {
+  d3.json("samples.json").then((data) => {
+    var samples = data.samples;
+    var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
