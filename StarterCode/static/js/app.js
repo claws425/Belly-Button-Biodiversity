b333da1f2bc84d16c4a0ebfecb059687296d12bb
claws425 <73760859+claws425@users.noreply.github.com>
2021-04-25 16:54:14 -0400

    added tags for each key-value

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,3 +9,10 @@ function buildMetadata(sample) {
 
       // Use `.html("") to clear any existing metadata
       PANEL.html("");
+      
+      // Use `Object.entries` to add each key and value pair to the panel
+      // Hint: Inside the loop, you will need to use d3 to append new
+      // tags for each key-value in the metadata.
+      Object.entries(result).forEach(([key, value]) => {
+        PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+      });
